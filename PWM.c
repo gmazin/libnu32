a4be483d54ca67b0919591b4b55f731cb1ced4ae
gmazin <gmazin@u.northwestern.edu>
2014-04-15 20:34:43 -0500

    PWM - added direction initialization, speed function

--- PWM.c
+++ PWM.c
@@ -1,50 +1,79 @@
 #include "NU32.h"
 
 void init_pwm(int pin) {
+  
   switch (pin) {
 	case D0:
+	  OC0CONbits.OCM = 0b110; // PWM mode without fault pin; other OC0CON bits are defaults
+	  OC0RS = 500; // duty cycle = OC1RS/(PR2+1) = 25%
+	  OC0R = 500; // initialize before turning OC0 on; then it is read-only
+	  OC0CONbits.ON = 1; // turn on OC0
+	  OC0RS = 0;
+	  break;	
+	case D1:
 	  OC1CONbits.OCM = 0b110; // PWM mode without fault pin; other OC0CON bits are defaults
 	  OC1RS = 500; // duty cycle = OC1RS/(PR2+1) = 25%
 	  OC1R = 500; // initialize before turning OC0 on; then it is read-only
 	  OC1CONbits.ON = 1; // turn on OC0
 	  OC1RS = 0;
-	  break;	
-	case D1:
+	  break;
+	case D2:
 	  OC2CONbits.OCM = 0b110; 
 	  OC2RS = 500; 
 	  OC2R = 500; 
 	  OC2CONbits.ON = 1; 
 	  OC2RS = 0;
 	  break;
-	case D2:
-	  OC3CONbits.OCM = 0b110; 
+	case D3:
+          OC3CONbits.OCM = 0b110; 
 	  OC3RS = 500; 
 	  OC3R = 500; 
 	  OC3CONbits.ON = 1; 
 	  OC3RS = 0;
 	  break;
-	case D3:
-          OC4CONbits.OCM = 0b110; 
-	  OC4RS = 500; 
-	  OC4R = 500; 
-	  OC4CONbits.ON = 1; 
-	  OC4RS = 0;
-	  break;
 	case D4:
-	  OC2CONbits.OCM = 0b110; 
+	  OC4CONbits.OCM = 0b110; 
 	  OC4RS = 500; 
 	  OC4R = 500; 
 	  OC4CONbits.ON = 1; 
 	  OC4RS = 0;
 	  break;
 	default:
+	//add serial message if possible
   	  break;
 
   }
 
 }
 
+void init_pwm_dir(int pwm_pin, int dir_pin) {
+  init_pin(pwm_pin);
+  set_pin(dir_pin, 0);
+}
 
+void set_speed(int pwm_pin, int speed) {
+  switch (pwm_pin) {
+	case D0:
+	  OC0RS = speed;
+	  break;	
+	case D1:
+	  OC1RS = speed;
+	  break;
+	case D2:
+	  OC2RS = speed;
+	  break;
+	case D3:
+	  OC3RS = speed;
+	  break;
+	case D4:
+	  OC4RS = speed;
+	  break;
+	default:
+	//add serial message if possible
+  	  break;
+  }
+}
+	
 /*
 void init_pwm_D4_dir_D5(void);
 void set_speed_D4_dir_D5(int speed, int dir);
