09578e38f57aa407f38214d518aefbb9dcc74c53
gmazin <gmazin@u.northwestern.edu>
2014-04-21 22:20:14 -0500

    main function seperation test

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "NU32.h"
 #include "PWM.h"
+#include "main.h"
 
-int main(void) {
-    startup();
-    init_pwm_dir(D4, D5);
-    set_speed(D4, 100);
-    while (1){
-        if (user_switch_pressed()) set_speed(D4, 0);
-    }
+void setup(){
+
+}
+
+void loop(){
+    
 }
