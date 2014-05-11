965d334aa4ce0cfbac01b657c45fbe0565a613e6
Kevin Ye <kevin.ye204@gmail.com>
2014-05-10 20:43:05 -0500

    Servo switched to Timer3

--- Servo.c
+++ Servo.c
@@ -8,13 +8,13 @@
 
 void init_rcservo(int pin)
 {
-    T2CON = 0; //turn off Timer2 when configuring it
+    T3CON = 0; //turn off Timer2 when configuring it
+    T3CONbits.TCKPS = 0b111; //prescaler N=256 (2^7)
+    TMR3 = 0; //initial Timer2 count to 0
+    PR3 = 6249; //the period will be (PR2+1)*N*12.5ns = 20ms, or 50Hz
     switch(pin){
         case D0:
             OC1CON = 0; //i guess turn off OC1 when configuring it too
-            T2CONbits.TCKPS = 0b111; //prescaler N=256 (2^7)
-            TMR2 = 0; //initial Timer2 count to 0
-            PR2 = 6249; //the period will be (PR2+1)*N*12.5ns = 20ms, or 50Hz
             OC1CONbits.OCM = 0b110;
             /*
              * 0 degrees = 0.5ms pulse = (6250/.02)*.0005 = 157
@@ -27,9 +27,6 @@ void init_rcservo(int pin)
             break;
         case D1:
             OC2CON = 0;
-            T2CONbits.TCKPS = 0b111;
-            TMR2 = 0;
-            PR2 = 6249;
             OC2CONbits.OCM = 0b110;
             OC2RS = 469;
             OC2R = 469;
@@ -37,9 +34,6 @@ void init_rcservo(int pin)
             break;
         case D2:
             OC3CON = 0;
-            T2CONbits.TCKPS = 0b111;
-            TMR2 = 0;
-            PR2 = 6249;
             OC3CONbits.OCM = 0b110;
             OC3RS = 469;
             OC3R = 469;
@@ -47,9 +41,6 @@ void init_rcservo(int pin)
             break;
         case D3:
             OC4CON = 0;
-            T2CONbits.TCKPS = 0b111;
-            TMR2 = 0;
-            PR2 = 6249;
             OC4CONbits.OCM = 0b110;
             OC4RS = 469;
             OC4R = 469;
@@ -57,9 +48,6 @@ void init_rcservo(int pin)
             break;
         case D4:
             OC5CON = 0;
-            T2CONbits.TCKPS = 0b111;
-            TMR2 = 0;
-            PR2 = 6249;
             OC5CONbits.OCM = 0b110;
             OC5RS = 469;
             OC5R = 469;
@@ -68,38 +56,36 @@ void init_rcservo(int pin)
         default:
             break;
     }
-    T2CONbits.ON = 1;
+    T3CONbits.ON = 1;
 }
 
 void set_rcservo(int pin, int degrees)
 {
     float set_valuef;
     int set_valuei;
+
+    set_valuef = degrees * 3.467+157.0;
+    set_valuei = set_valuef;
+
     switch(pin){
         case D0:
-            set_valuef = degrees * 3.467+157.0;
-            set_valuei = set_valuef;
             OC1RS = set_valuei;
             //OC1RS = (int)(157 + ((degrees/180.0) * 624));
             break;
         case D1:
-            set_valuef = degrees * 3.467+157.0;
-            set_valuei = set_valuef;
+            OC2RS = set_valuei;
             //OC2RS = (int)(157 + ((degrees/180.0) * 624));
             break;
         case D2:
-            set_valuef = degrees * 3.467+157.0;
-            set_valuei = set_valuef;
+            OC3RS = set_valuei;
             //OC3RS = (int)(157 + ((degrees/180.0) * 624));
             break;
         case D3:
-            set_valuef = degrees * 3.467+157.0;
-            set_valuei = set_valuef;
+            OC4RS = set_valuei;
             //OC4RS = (int)(157 + ((degrees/180.0) * 624));
             break;
         case D4:
-            set_valuef = degrees * 3.467+157.0;
-            set_valuei = set_valuef;
+            OC5RS = set_valuei;
             //OC5RS = (int)(157 + ((degrees/180.0) * 624));
             break;
         default:
